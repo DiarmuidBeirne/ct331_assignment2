dd6ffe8c444429bcce2b4dea443775e599d845c0
Diarmuid Beirne <d.beirne.8@gmail.com>
2017-10-24 14:08:44 +0100

    Question2 started

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -12,6 +12,28 @@
 ;You may delete these comments!
 
 (provide ins_beg)
+(provide ins_end)
+(provide count_top_level)
+(provide count_instances)
 
+;A
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+ (append (list el) lst))
+
+;B
+(define (ins_end el lst)
+ (append lst (list el)))
+
+;C
+(define (count_top_level lst)
+ (if (null? lst)
+      0
+      (+ 1 (count_top_level (cdr lst)))))
+
+;D
+(define (count_instances item lst)
+  (cond [(null? lst) 0]
+        [(equal? item (car lst)) (+ 1 (count_instances item (cdr lst)))]
+        [else (count_instances item (cdr lst))]))
+
+
