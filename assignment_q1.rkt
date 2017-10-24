e52dc929dd75c27695c02815ffa993e6be7d5de7
Diarmuid Beirne <d.beirne.8@gmail.com>
2017-10-24 13:03:35 +0100

    Q1 Complete

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,18 @@
 #lang racket
+
+(define cons_pair(cons 4 12))
+
+(define cons_list(cons 2 (cons 1 (cons 6 empty))))
+
+(define cons_list_2(cons "hello" (cons 2 (cons (cons 1 (cons 5 (cons 10 empty))) '()))))
+
+(define list1 (list "hello!" 2 (list 1 5 10)))
+
+(define list2(append '("string!") '(1) '((2 3 4))))
+
+
+
+
+
+
+
